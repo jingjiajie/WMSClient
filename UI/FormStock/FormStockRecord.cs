69958672b6260484c6b7ca3b6e5be10535a22a73
hsjjaaaa <a098100@163.com>
2018-05-22 20:53:56 +0800

    add form stock record

---
+++ UI/FormStock/FormStockRecord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WMS.UI.FormStock
+{
+    public partial class FormStockRecord : Form
+    {
+        public FormStockRecord()
+        {
+            InitializeComponent();
+        }
+
+        private void FormStockRecord_Load(object sender, EventArgs e)
+        {
+            //设置两个请求参数
+            this.synchronizer.SetRequestParameter("$url", Defines.ServerURL);
+            this.synchronizer.SetRequestParameter("$accountBook", GlobalData.AccountBook);
+            this.searchView1.Search();
+        }
+
+        private void toolStripButtonAdd_Click(object sender, EventArgs e)
+        {
+            this.model1.InsertRow(0, null);                    
+        }
+
+        private void toolStripButtonDelete_Click(object sender, EventArgs e)
+        {
+            this.model1.RemoveSelectedRows();
+        }
+
+        private void toolStripButtonAlter_Click(object sender, EventArgs e)
+        {
+            this.synchronizer.Save();
+        }
+    }
+}
