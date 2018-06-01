f4d74aa9d340a860c647c5208f3f25559f91036b
a098100 <a098100@163.com>
2018-06-01 15:09:49 +0800

    123

--- UI/Defines.cs
+++ UI/Defines.cs
@@ -9,8 +9,8 @@ namespace WMS.UI
     {
 
         //public static string ServerURL = "http://localhost.fiddler:9000";
-        //public static string ServerURL = "http://localhost:9000";
-        public const string ServerURL = "http://linuxserver.jingjiajie.com:9000";
+        public static string ServerURL = "http://localhost:9000";
+        //public const string ServerURL = "http://linuxserver.jingjiajie.com:9000";
 
     }
 }
