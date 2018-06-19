2b8b61eacc362415a5608e5044dc532ee0194aab
hsjjaaaa <a098100@163.com>
2018-06-19 09:19:17 +0800

    fix in stock taking

--- UI/FormStock/FormStockTakingOrderItem.Designer.cs
+++ UI/FormStock/FormStockTakingOrderItem.Designer.cs
@@ -144,10 +144,12 @@
             // model1
             // 
             this.model1.AllSelectionRanges = new FrontWork.Range[0];
+            this.model1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.model1.Configuration = this.configuration1;
             this.model1.CurrentModelName = "default";
+            this.model1.Font = new System.Drawing.Font("宋体", 10F);
             this.model1.Location = new System.Drawing.Point(279, 27);
-            this.model1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.model1.Margin = new System.Windows.Forms.Padding(2);
             this.model1.Mode = "default";
             this.model1.Name = "model1";
             this.model1.SelectionRange = null;
