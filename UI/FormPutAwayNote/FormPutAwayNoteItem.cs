04893e3ba89993c8ed0eb24ee94399f0e5152bed
陈光晃 <wingmonth@outlook.com>
2018-07-07 14:03:54 +0800

    1

--- UI/FormPutAwayNote/FormPutAwayNoteItem.cs
+++ UI/FormPutAwayNote/FormPutAwayNoteItem.cs
@@ -294,21 +294,21 @@ namespace WMS.UI
 
         //===========为了实现一个看起来天经地义的交互逻辑=========
 
-        private void SupplierNoEditEnded(int row)
-        {
-            if (string.IsNullOrWhiteSpace(this.model1[row, "supplierNo"]?.ToString())) return;
-            this.model1[row, "supplierName"] = "";
-            this.FindSupplierID(row);
-            this.TryGetSupplyID(row);
-        }
+        //private void SupplierNoEditEnded(int row)
+        //{
+        //    if (string.IsNullOrWhiteSpace(this.model1[row, "supplierNo"]?.ToString())) return;
+        //    this.model1[row, "supplierName"] = "";
+        //    this.FindSupplierID(row);
+        //    this.TryGetSupplyID(row);
+        //}
 
-        private void SupplierNameEditEnded(int row)
-        {
-            if (string.IsNullOrWhiteSpace(this.model1[row, "supplierName"]?.ToString())) return;
-            this.model1[row, "supplierNo"] = "";
-            this.FindSupplierID(row);
-            this.TryGetSupplyID(row);
-        }
+        //private void SupplierNameEditEnded(int row)
+        //{
+        //    if (string.IsNullOrWhiteSpace(this.model1[row, "supplierName"]?.ToString())) return;
+        //    this.model1[row, "supplierNo"] = "";
+        //    this.FindSupplierID(row);
+        //    this.TryGetSupplyID(row);
+        //}
 
         private void MaterialNoEditEnded(int row)
         {
