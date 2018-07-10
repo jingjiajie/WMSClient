db285ec2367b0877b08fdcddd2db89b05aeb9410
陈光晃 email = wingmonth@outlook.com <wingmonth@outlook.com>
2018-07-10 12:30:06 +0800

    1

--- UI/FormAcccount/FormTax.cs
+++ UI/FormAcccount/FormTax.cs
@@ -33,7 +33,7 @@ namespace WMS.UI.FormAcccount
         {
             if (this.synchronizer.Save())
             {
-                //this.searchView1.Search();
+                this.searchView1.Search();
                 //Condition condWarehouse = new Condition().AddCondition("warehouseId", GlobalData.Warehouse["id"]);
                 //GlobalData.AllMaterials = RestClient.Get<List<IDictionary<string, object>>>(
                 //   $"{Defines.ServerURL}/warehouse/{GlobalData.AccountBook}/material/{condWarehouse.ToString()}");
@@ -49,7 +49,18 @@ namespace WMS.UI.FormAcccount
 
         private void buttonItems_Click(object sender, EventArgs e)
         {
-
+            if (this.model1.SelectionRange.Rows != 1)
+            {
+                MessageBox.Show("请选择一项税务查看税务详情页！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            var tax = this.model1.GetRows(new int[] { this.model1.SelectionRange.Row })[0];
+            if (tax["id"] == null)
+            {
+                MessageBox.Show("请先保存单据再查看税务详情页！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            new FormTaxItem(tax).Show();
         }
     }
 }
