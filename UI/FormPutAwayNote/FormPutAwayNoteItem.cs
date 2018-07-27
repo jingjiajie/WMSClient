d7bf127907e8f7e88f3f87c6cac3c5f717109c3f
陈光晃 email = wingmonth@outlook.com <wingmonth@outlook.com>
2018-07-27 13:25:34 +0800

    1

--- UI/FormPutAwayNote/FormPutAwayNoteItem.cs
+++ UI/FormPutAwayNote/FormPutAwayNoteItem.cs
@@ -97,17 +97,13 @@ namespace WMS.UI
         //部分完成
         private void buttonFinish_Click(object sender, EventArgs e)
         {
-            if (this.synchronizer.Save())
+            if (MessageBox.Show("确认保存当前修改并完成选中条目吗？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes) return;
+            //如果存在更新并且未保存的行，则先尝试保存
+            if (this.model1.HasUnsynchronizedUpdatedRow())
             {
-                this.searchView1.Search();
+                if (!this.synchronizer.Save()) return;
             }
-            //if (MessageBox.Show("确认保存当前修改并完成选中条目吗？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes) return;
-            ////如果存在更新并且未保存的行，则先尝试保存
-            //if (this.model1.HasUnsynchronizedUpdatedRow())
-            //{
-            //    if (!this.synchronizer.Save()) return;
-            //}
-            //this.TransferDone();
+            this.TransferDone();
         }
 
         private string ScheduledAmountForwardMapper([Data]double amount, [Row]int row)
