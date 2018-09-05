193420f39f4388cf87a4f66cbee5a5752a4c8d6a
hsjjaaaa <a098100@163.com>
2018-09-05 17:53:07 +0800

    formSummaryNote

---
+++ UI/FormSettlement/FormSummaryNote.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WMS.UI.FormSettlement
+{
+    public partial class FormSummaryNote : Form
+    {
+        public FormSummaryNote()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButtonAdd_Click(object sender, EventArgs e)
+        {
+            this.model1.InsertRow(0, new Dictionary<string, object>()
+            {             
+                { "createPersonId",GlobalData.Person["id"]},
+                { "createPersonName",GlobalData.Person["name"]},            
+                { "createTime",DateTime.Now},
+            });
+        }
+
+        private void toolStripButtonDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("确认删除吗？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes) return;
+            this.model1.RemoveSelectedRows();
+        }
+
+        private void toolStripButtonAlter_Click(object sender, EventArgs e)
+        {
+            if (this.synchronizer.Save()) { this.searchView1.Search(); }
+        }
+
+        private void buttonItems_Click(object sender, EventArgs e)
+        {
+            if (this.model1.SelectionRange==null) { return; }
+            if (this.model1.SelectionRange.Rows != 1)
+            {
+                MessageBox.Show("请选择一项汇总单查看盘点单条目！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            var rowData = this.model1.GetRows(new int[] { this.model1.SelectionRange.Row })[0];
+            //var a1 = new FormStockTakingOrderItem(rowData);
+            //a1.Show();
+        }
+    }
+}
