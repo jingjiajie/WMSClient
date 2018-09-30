1854e10cf2d217e003c06ed7e3a76d1978122aa2
hsjjaaaa <a098100@163.com>
2018-09-30 10:36:54 +0800

    fix

--- UI/FormSettlement/FormTrayThresholds.cs
+++ UI/FormSettlement/FormTrayThresholds.cs
@@ -15,5 +15,25 @@ namespace WMS.UI.FormSettlement
         {
             InitializeComponent();
         }
+
+        private void toolStripButtonAdd_Click(object sender, EventArgs e)
+        {
+            this.model1.InsertRow(0, new Dictionary<string, object>()
+            {
+                { "warehouseId",GlobalData.Warehouse["id"]},
+                { "warehouseName",GlobalData.Warehouse["name"]}
+            });
+        }
+
+        private void toolStripButtonDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("确认删除吗？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes) return;
+            this.model1.RemoveSelectedRows();
+        }
+
+        private void toolStripButtonAlter_Click(object sender, EventArgs e)
+        {
+            if (this.synchronizer.Save()) { this.searchView1.Search(); }
+        }
     }
 }
