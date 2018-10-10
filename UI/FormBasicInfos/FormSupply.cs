27364b5aac3e692e3cfd4c99f07af86bfcf714cb
陈光晃 email = wingmonth@outlook.com <wingmonth@outlook.com>
2018-10-10 14:59:08 +0800

    1

--- UI/FormBasicInfos/FormSupply.cs
+++ UI/FormBasicInfos/FormSupply.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Windows.Forms;
 using ZXing;
 using ZXing.Common;
+using Microsoft.VisualBasic;
 
 namespace WMS.UI.FormBasicInfos
 {
@@ -19,34 +20,6 @@ namespace WMS.UI.FormBasicInfos
             MethodListenerContainer.Register(this);
             InitializeComponent();
             this.model1.CellUpdated += this.model_CellUpdated;
-            this.model1.RowRemoved += this.model_RowRemoved;
-            this.model1.Refreshed += this.model_Refreshed;
-        }
-
-        private void model_Refreshed(object sender, ModelRefreshedEventArgs e)
-        {
-            this.updateBasicAndReoGridView();
-        }
-
-        private void updateBasicAndReoGridView()
-        {
-
-            if (this.model1.RowCount == 0)
-            {
-                this.basicView1.Enabled = false;
-                this.reoGridView1.Enabled = false;
-            }
-            else
-            {
-                this.basicView1.Enabled = true;
-                this.reoGridView1.Enabled = true;
-            }
-
-        }
-
-        private void model_RowRemoved(object sender, ModelRowRemovedEventArgs e)
-        {
-            this.updateBasicAndReoGridView();
         }
 
         private void model_CellUpdated(object sender, ModelCellUpdatedEventArgs e)
@@ -66,13 +39,11 @@ namespace WMS.UI.FormBasicInfos
             this.synchronizer.SetRequestParameter("$accountBook", GlobalData.AccountBook);
             this.searchView1.AddStaticCondition("warehouseId", GlobalData.Warehouse["id"]);
             this.searchView1.Search();
-            this.updateBasicAndReoGridView();
         }
 
         private void toolStripButtonAdd_Click(object sender, EventArgs e)
         {
-            this.basicView1.Enabled = true;
-            this.reoGridView1.Enabled = true;
+            //Interaction.InputBox();
             this.model1.InsertRow(0, new Dictionary<string, object>()
             {
                 { "warehouseId",GlobalData.Warehouse["id"]},
