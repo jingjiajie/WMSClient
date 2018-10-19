34c961facde819998bb18eceb9716834cb10f636
a098100 <a098100@163.com>
2018-10-20 00:38:49 +0800

    fix

--- UI/Defines.cs
+++ UI/Defines.cs
@@ -8,10 +8,10 @@ namespace WMS.UI
     class Defines
     {
         //public static string ServerURL = "http://localhost.fiddler:9000";
-         public static string ServerURL = "http://localhost:9000";
+        // public static string ServerURL = "http://localhost:9000";
         //public const string ServerURL = "http://linuxserver.jingjiajie.com:9000";
 
         //正式服，非请勿进
-        //public const string ServerURL = "http://47.93.35.199:9000";
+        public const string ServerURL = "http://47.93.35.199:9000";
     }
 }
