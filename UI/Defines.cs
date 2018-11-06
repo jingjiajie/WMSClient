ff0cdc50387b215b2193c66991140931db21ba34
a098100 <a098100@163.com>
2018-11-06 23:14:45 +0800

    fix

--- UI/Defines.cs
+++ UI/Defines.cs
@@ -13,6 +13,6 @@ namespace WMS.UI
 
         //正式服，非请勿进
         //※调试时进入后请先改回测试服地址再推！※
-        //public const string ServerURL = "http://47.93.35.199:9000";
+       // public const string ServerURL = "http://47.93.35.199:9000";
     }
 }
