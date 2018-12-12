df9e500439ba5b2efed89497bb895fbd3cf9789a
陈光晃 email = wingmonth@outlook.com <wingmonth@outlook.com>
2018-12-12 18:36:14 +0800

    fix

---
+++ UI/FormSettlement/SettlementNoteState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WMS.UI.FormSettlement
+{
+    class SettlementNoteState
+    {
+        public const int NoComfirm = 0;//待确认
+        public const int Receivables = 1;//应收
+        public const int Receipts = 2;//实收
+    }
+}
