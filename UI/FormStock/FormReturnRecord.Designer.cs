e02e4543caffcf95ae2d47582501c8ad298d1b49
hsjjaaaa <a098100@163.com>
2019-01-24 11:27:04 +0800

    return record

---
+++ UI/FormStock/FormReturnRecord.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WMS.UI.FormStock
+{
+    partial class FormReturnRecord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormReturnRecord));
+            FrontWork.ModeMethodListenerNamesPair modeMethodListenerNamesPair1 = new FrontWork.ModeMethodListenerNamesPair();
+            FrontWork.SearchViewJsonRESTAdapter.APIParamNamesType apiParamNamesType1 = new FrontWork.SearchViewJsonRESTAdapter.APIParamNamesType();
+            FrontWork.SearchViewJsonRESTAdapter.ConditionFieldNamesType conditionFieldNamesType1 = new FrontWork.SearchViewJsonRESTAdapter.ConditionFieldNamesType();
+            FrontWork.SearchViewJsonRESTAdapter.OrderParamNamesType orderParamNamesType1 = new FrontWork.SearchViewJsonRESTAdapter.OrderParamNamesType();
+            this.basicView1 = new FrontWork.BasicView();
+            this.configuration1 = new FrontWork.Configuration();
+            this.model1 = new FrontWork.Model();
+            this.pagerView1 = new FrontWork.PagerView();
+            this.panelSearchWidget = new System.Windows.Forms.Panel();
+            this.panelPager = new System.Windows.Forms.Panel();
+            this.pagerSearchJsonRESTAdapter1 = new FrontWork.PagerSearchJsonRESTAdapter();
+            this.synchronizer = new FrontWork.JsonRESTSynchronizer();
+            this.reoGridView1 = new FrontWork.ReoGridView();
+            this.labelStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.searchView1 = new FrontWork.SearchView();
+            this.panelPager.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // basicView1
+            // 
+            this.basicView1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.basicView1.Configuration = this.configuration1;
+            this.basicView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.basicView1.Font = new System.Drawing.Font("黑体", 10F);
+            this.basicView1.ItemsPerRow = 5;
+            this.basicView1.Location = new System.Drawing.Point(0, 31);
+            this.basicView1.Margin = new System.Windows.Forms.Padding(0);
+            this.basicView1.Mode = "default";
+            this.basicView1.Model = this.model1;
+            this.basicView1.Name = "basicView1";
+            this.basicView1.Padding = new System.Windows.Forms.Padding(3);
+            this.basicView1.Size = new System.Drawing.Size(893, 94);
+            this.basicView1.TabIndex = 13;
+            // 
+            // configuration1
+            // 
+            this.configuration1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.configuration1.ConfigurationString = resources.GetString("configuration1.ConfigurationString");
+            this.configuration1.Location = new System.Drawing.Point(79, 55);
+            this.configuration1.Margin = new System.Windows.Forms.Padding(2);
+            modeMethodListenerNamesPair1.MethodListenerNames = new string[] {
+        "FormReturnSupply",
+        "SupplyMethodListener",
+        "AssociationMethodListener"};
+            modeMethodListenerNamesPair1.Mode = "default";
+            this.configuration1.MethodListeners = new FrontWork.ModeMethodListenerNamesPair[] {
+        modeMethodListenerNamesPair1};
+            this.configuration1.Name = "configuration1";
+            this.configuration1.Size = new System.Drawing.Size(165, 125);
+            this.configuration1.TabIndex = 1;
+            // 
+            // model1
+            // 
+            this.model1.AllSelectionRanges = new FrontWork.Range[0];
+            this.model1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.model1.Configuration = this.configuration1;
+            this.model1.Font = new System.Drawing.Font("宋体", 10F);
+            this.model1.Location = new System.Drawing.Point(270, 57);
+            this.model1.Margin = new System.Windows.Forms.Padding(2);
+            this.model1.Mode = "default";
+            this.model1.Name = "model1";
+            this.model1.SelectionRange = null;
+            this.model1.Size = new System.Drawing.Size(117, 125);
+            this.model1.TabIndex = 2;
+            // 
+            // pagerView1
+            // 
+            this.pagerView1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pagerView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pagerView1.Location = new System.Drawing.Point(0, 452);
+            this.pagerView1.Margin = new System.Windows.Forms.Padding(0);
+            this.pagerView1.Mode = "default";
+            this.pagerView1.Name = "pagerView1";
+            this.pagerView1.PageSize = 50;
+            this.pagerView1.Size = new System.Drawing.Size(893, 26);
+            this.pagerView1.TabIndex = 14;
+            // 
+            // panelSearchWidget
+            // 
+            this.panelSearchWidget.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panelSearchWidget.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelSearchWidget.Location = new System.Drawing.Point(0, 0);
+            this.panelSearchWidget.Margin = new System.Windows.Forms.Padding(0);
+            this.panelSearchWidget.Name = "panelSearchWidget";
+            this.panelSearchWidget.Size = new System.Drawing.Size(893, 1);
+            this.panelSearchWidget.TabIndex = 12;
+            // 
+            // panelPager
+            // 
+            this.panelPager.BackColor = System.Drawing.SystemColors.Control;
+            this.panelPager.Controls.Add(this.pagerSearchJsonRESTAdapter1);
+            this.panelPager.Controls.Add(this.synchronizer);
+            this.panelPager.Controls.Add(this.model1);
+            this.panelPager.Controls.Add(this.configuration1);
+            this.panelPager.Controls.Add(this.reoGridView1);
+            this.panelPager.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPager.Location = new System.Drawing.Point(0, 125);
+            this.panelPager.Margin = new System.Windows.Forms.Padding(0);
+            this.panelPager.Name = "panelPager";
+            this.panelPager.Size = new System.Drawing.Size(893, 327);
+            this.panelPager.TabIndex = 8;
+            // 
+            // pagerSearchJsonRESTAdapter1
+            // 
+            conditionFieldNamesType1.Key = "key";
+            conditionFieldNamesType1.Relation = "relation";
+            conditionFieldNamesType1.Values = "values";
+            apiParamNamesType1.ConditionParamNames = conditionFieldNamesType1;
+            orderParamNamesType1.Key = "key";
+            orderParamNamesType1.Order = "order";
+            apiParamNamesType1.OrderParamNames = orderParamNamesType1;
+            this.pagerSearchJsonRESTAdapter1.APIFieldNames = apiParamNamesType1;
+            this.pagerSearchJsonRESTAdapter1.Location = new System.Drawing.Point(606, 55);
+            this.pagerSearchJsonRESTAdapter1.Margin = new System.Windows.Forms.Padding(2);
+            this.pagerSearchJsonRESTAdapter1.Name = "pagerSearchJsonRESTAdapter1";
+            this.pagerSearchJsonRESTAdapter1.PagerView = this.pagerView1;
+            this.pagerSearchJsonRESTAdapter1.SearchView = null;
+            this.pagerSearchJsonRESTAdapter1.Size = new System.Drawing.Size(175, 144);
+            this.pagerSearchJsonRESTAdapter1.Synchronizer = this.synchronizer;
+            this.pagerSearchJsonRESTAdapter1.TabIndex = 4;
+            this.pagerSearchJsonRESTAdapter1.Visible = false;
+            // 
+            // synchronizer
+            // 
+            this.synchronizer.Configuration = this.configuration1;
+            this.synchronizer.FieldMapping = new FrontWork.JsonRESTSynchronizer.FieldMappingItem[0];
+            this.synchronizer.Location = new System.Drawing.Point(426, 57);
+            this.synchronizer.Margin = new System.Windows.Forms.Padding(0);
+            this.synchronizer.Mode = "default";
+            this.synchronizer.Model = this.model1;
+            this.synchronizer.Name = "synchronizer";
+            this.synchronizer.Size = new System.Drawing.Size(147, 112);
+            this.synchronizer.TabIndex = 3;
+            // 
+            // reoGridView1
+            // 
+            this.reoGridView1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.reoGridView1.Configuration = this.configuration1;
+            this.reoGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reoGridView1.Font = new System.Drawing.Font("黑体", 11F);
+            this.reoGridView1.Location = new System.Drawing.Point(0, 0);
+            this.reoGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.reoGridView1.Mode = "default";
+            this.reoGridView1.Model = this.model1;
+            this.reoGridView1.Name = "reoGridView1";
+            this.reoGridView1.Size = new System.Drawing.Size(893, 327);
+            this.reoGridView1.TabIndex = 0;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(32, 21);
+            this.labelStatus.Text = "退货";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(44, 21);
+            this.toolStripStatusLabel1.Text = "状态：";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.labelStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 478);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 12, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(893, 26);
+            this.statusStrip1.TabIndex = 19;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.searchView1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.statusStrip1, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.panelSearchWidget, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panelPager, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pagerView1, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.basicView1, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 0F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 94F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(893, 524);
+            this.tableLayoutPanel1.TabIndex = 23;
+            // 
+            // searchView1
+            // 
+            this.searchView1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.searchView1.Configuration = this.configuration1;
+            this.searchView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.searchView1.Location = new System.Drawing.Point(2, 2);
+            this.searchView1.Margin = new System.Windows.Forms.Padding(2);
+            this.searchView1.Mode = "default";
+            this.searchView1.Name = "searchView1";
+            this.searchView1.Size = new System.Drawing.Size(889, 27);
+            this.searchView1.TabIndex = 5;
+            // 
+            // FormReturnRecord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 524);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FormReturnRecord";
+            this.Text = "退货记录";
+            this.panelPager.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FrontWork.BasicView basicView1;
+        private FrontWork.Configuration configuration1;
+        private FrontWork.Model model1;
+        private FrontWork.PagerView pagerView1;
+        private System.Windows.Forms.Panel panelSearchWidget;
+        private System.Windows.Forms.Panel panelPager;
+        private FrontWork.PagerSearchJsonRESTAdapter pagerSearchJsonRESTAdapter1;
+        private FrontWork.JsonRESTSynchronizer synchronizer;
+        private FrontWork.ReoGridView reoGridView1;
+        private System.Windows.Forms.ToolStripStatusLabel labelStatus;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private FrontWork.SearchView searchView1;
+    }
+}
