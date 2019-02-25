76bacf01c6fdb9e068dfe758f46064742c56e843
hsjjaaaa <a098100@163.com>
2019-02-25 20:28:26 +0800

    update

--- UI/FormMain/FormMain.Designer.cs
+++ UI/FormMain/FormMain.Designer.cs
@@ -119,7 +119,7 @@ namespace WMS.UI
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(501, 30);
             this.label1.TabIndex = 0;
-            this.label1.Text = "安途丰达WMS仓库管理系统 V2.2.2- Build0122";
+            this.label1.Text = "安途丰达WMS仓库管理系统 V2.2.3- Build0225";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel1
